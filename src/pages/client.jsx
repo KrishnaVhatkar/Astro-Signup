acba927ae217e6c05fcd797b62c9a8358a5ba8e5
KrishnaVhatkar <119169766+KrishnaVhatkar@users.noreply.github.com>
2024-01-18 13:11:43 +0530

    first commit

--- src/pages/client.jsx
+++ src/pages/client.jsx
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://tgxeezsumvabjyazhexw.supabase.co";
+const supabaseUrl = "https://guhgadeecqnjvdrfgroc.supabase.co";
 const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRneGVlenN1bXZhYmp5YXpoZXh3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDUwMzUwNzIsImV4cCI6MjAyMDYxMTA3Mn0.xEAjNJ6aJh6vpL-ZwZsQ717MstDoOrJkIEqNhOX15nc";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd1aGdhZGVlY3FuanZkcmZncm9jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDU1NTU1NjMsImV4cCI6MjAyMTEzMTU2M30.P7Sa9V5P6OlzcSXJ_EICMMQsmenMEzbUjrwYcWQ5rUI";
 export const supabase = createClient(supabaseUrl, supabaseKey);
